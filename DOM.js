06c16bb25a7f3b966d5411a0fe7eac5ea7fd83af
ElizaKnapp <eknapp20@stuy.edu>
2021-01-13 21:38:02 -0500

    see libraries for more specifics but we're using react anyway

---
+++ DOM.js
@@ -0,0 +1,9 @@
+console.log(window);
+window.alert(1);
+
+//Single element selectors- select parts of the html (dom)
+const form = document.getElementByID('my-form');
+console.log(form);
+
+//multiple elements
+console.log(document.querySelectorALl('.item'));
